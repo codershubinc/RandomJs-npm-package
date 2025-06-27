f97837fecb4c3dcfe0157dddbbbf6469ad7addc1
Swapnil ingle <ingleswapnil2004@gmail.com>
2025-06-27 21:48:54 +0530

    Implement getRandomNumber function and export it

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello, npm!");
+function getRandomNumber(min = 0, max = 100) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default {
+    getRandomNumber
+};
